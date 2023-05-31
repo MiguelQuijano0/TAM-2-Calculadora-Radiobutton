102ae137285f88211477c14595a70f60d09c18a0
miguelquijano0 <mikeangelska8@gmail.com>
2023-05-31 11:54:59 -0600

    agrego la potencia de un numero

--- app/src/main/java/com/example/tam_2_calculadora_radiobutton/MainActivity.java
+++ app/src/main/java/com/example/tam_2_calculadora_radiobutton/MainActivity.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private EditText et1,et2;
     private TextView tv1;
-    private RadioButton r1,r2,r3,r4;
+    private RadioButton r1,r2,r3,r4,r5;
 
 
     @Override
@@ -27,37 +27,52 @@ public class MainActivity extends AppCompatActivity {
         r2 =(RadioButton)findViewById(R.id.r2);
         r3 =(RadioButton)findViewById(R.id.r3);
         r4 =(RadioButton)findViewById(R.id.r4);
+        r5 =(RadioButton)findViewById(R.id.r5);
     }
     public void operar(View view) {
         String valor1=et1.getText().toString();
         String valor2=et2.getText().toString();
         int nro1=Integer.parseInt(valor1);
         int nro2=Integer.parseInt(valor2);
+
         if (r1.isChecked()==true) {
             int suma=nro1+nro2;
             String resu=String.valueOf(suma);
             tv1.setText(resu);
         } else
-        if (r2.isChecked()==true) {
-            int resta=nro1-nro2;
-            String resu=String.valueOf(resta);
-            tv1.setText(resu);
-        } else
-        if (r3.isChecked()==true) {
-            int multiplicar=nro1*nro2;
-            String resu=String.valueOf(multiplicar);
-            tv1.setText(resu);
-        } else
-        if (r4.isChecked()==true) {
-            String error= "ingrese un numero mayor de 0";
-            if (nro2>0){
-                int dividir=nro1/nro2;
-                String resu=String.valueOf(dividir);
+            if (r2.isChecked()==true) {
+                int resta=nro1-nro2;
+                String resu=String.valueOf(resta);
                 tv1.setText(resu);
-            }else
-            if (nro2==0){
-                tv1.setText(error);
-            }
-        }
+            } else
+                if (r3.isChecked()==true) {
+                    int multiplicar=nro1*nro2;
+                    String resu=String.valueOf(multiplicar);
+                    tv1.setText(resu);
+                } else
+                    if (r4.isChecked()==true) {
+                        String error= "ingrese un numero mayor de 0";
+                        if (nro2>0){
+                            int dividir=nro1/nro2;
+                            String resu=String.valueOf(dividir);
+                            tv1.setText(resu);
+                        }else
+                            if (nro2==0){
+                                tv1.setText(error);
+                            }
+                    }else
+                        if (r5.isChecked()==true){
+                            if (nro2>=0){
+                                int exponente = nro2;
+                                int potencia = 1;
+                                while(0<exponente){
+                                    potencia = potencia*nro1;
+                                    exponente--;
+                                }
+                                String resu=String.valueOf(potencia);
+                                tv1.setText(resu);
+                            }
+
+                        }
     }
 }
